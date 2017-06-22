ba9a21d0f2aca1da2c2ce98fc19dbc23f9308907
asheesh55555 <asheeshvits@gmail.com>
2017-06-22 15:45:42 +0530

    bbb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   
   
   resources :articles
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "home#index"
 get "/abc/xyz" => 'posts#profile_detail'
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620124519) do
+ActiveRecord::Schema.define(version: 20170622080439) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20170620124519) do
     t.string "mobile_number"
     t.date "birth_day"
     t.string "gender"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
